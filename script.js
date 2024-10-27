291e5e6b244166ad7156203ffedd9621ec561ff8
jessesrekdev <159046986+jessesrekdev@users.noreply.github.com>
2024-10-27 14:08:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ import('./dw.js');
 r.style.display = 'none';
 cardbody.style.display = 'none';
 
-start.addEventListener('click', function(){
+start.addEventListener('click', function () {
     cardbody.style.display = 'block';
     start.style.display = 'none';
 });
 
-r.addEventListener('click', function() {
+r.addEventListener('click', function () {
     location.reload();
+});
+
+faq.addEventListener('click', function () {
+    window.open("http://127.0.0.1:5500/faq");
 });
