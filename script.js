627102f5ab7a569fa8925cca9d14d3e365a2083e
jessesrekdev <159046986+jessesrekdev@users.noreply.github.com>
2024-11-05 01:29:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,5 +13,5 @@ r.addEventListener('click', function () {
 });
 
 faq.addEventListener('click', function () {
-    window.open("http://127.0.0.1:5500/faq");
+    window.open("http://youtube.jesse-network.com/faq");
 });
